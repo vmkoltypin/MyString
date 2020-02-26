307654d381ec80d977e4e3a456b2ce3b962d1c27
Всеволод Колтыпин <vmkoltypin@edu.tversu.ru>
2020-02-26 11:30:22 +0300

    git2

--- MyString/MyString/String.cpp
+++ MyString/MyString/String.cpp
@@ -1,1 +1,30 @@
 #include "String.h"
+#include <iostream>
+
+
+String::String()
+{
+	str = NULL;
+	n = 0;
+}
+
+String::String(const String& s)
+{
+	n = s.n;
+	str = new char[n + 1];
+	strcpy_s(str, n + 1, s.str);
+}
+
+String::String(char* s)
+{
+	n = strlen(s);
+	str = new char[n + 1];
+	strcpy_s(str, n + 1, s);
+}
+
+String::String(const char* s)
+{
+	n = strlen(s);
+	str = new char[n + 1];
+	strcpy_s(str, n + 1, s);
+}
--- MyString/MyString/String.h
+++ MyString/MyString/String.h
@@ -1,5 +1,15 @@
 #pragma once
 class String
 {
+private:
+	char* str;
+	int n;
+public:
+	String();
+	String(const String& s);
+	String(char* s);
+	String(const char* s);
 };
 
+//принимаем только строку в cpp пишем, смотрим на длину строки и передаём
+//в стр с найденой длиной строки 
