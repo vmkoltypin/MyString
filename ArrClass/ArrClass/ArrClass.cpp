3402e5c0c1ffe2732325c3ce7dc8217770432af3
Всеволод Колтыпин <vmkoltypin@edu.tversu.ru>
2020-03-04 11:39:52 +0300

    bublesort

---
+++ ArrClass/ArrClass/ArrClass.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+#include"Header.h"
+using namespace std;
+int main()
+{
+	int a[5] = {1,2,5,3,2};
+	Array<int> arr(a,5);
+	Array<int> b;
+	b = arr;
+	arr[0] = 5;
+	arr.bublesort();
+	//f(n) = 4;
+	cout << arr << endl;
+	cout << b << endl;
+}
+
